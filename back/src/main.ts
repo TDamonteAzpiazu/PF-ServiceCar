01111bc72b0ffde46a537f98ab167d825b306ac5
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-08-19 19:24:28 -0300

    Carpeta users con su entidad y archivos

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ServiceCar')
+    .setDescription('Este es un proyecto para el último módulo de Henry, consiste de una web de contratación de servicios para autos.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
