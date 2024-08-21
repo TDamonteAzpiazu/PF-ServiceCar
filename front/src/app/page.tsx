7dc1a85f61881f41bc06151f8922ce934e4aa084
Agustin Haag <agustin-haag@hotmail.com>
2024-08-21 09:45:04 -0300

    landing

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,38 @@
+import Banner from "@/components/landing/Banner";
+import React from "react";
+import { IoSearchSharp } from "react-icons/io5";
 
-export default function Home() {
+const Landing = () => {
   return (
-  <main>
+    <main>
+      <Banner />
+      <section>
+        <p>Trabajo y eficiencia</p>
+        <h2>GARAGEJS</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          sint harum illo
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad dolor
+          sunt natus facilis maxime minima quia praesentium, numquam cumque
+          fugiat cum optio accusamus corrupti odio unde quam ea fuga nulla.
+        </p>
+        <div className="flex">
+          <div className="relative flex w-60 bg-custom-grey items-center">
+            <input type="text" placeholder="Ingrese su localidad" className="bg-transparent outline-none"/>
+            <span className="absolute right-1">
+              <IoSearchSharp />
+            </span>
+          </div>
+          <button>Buscar</button>
+        </div>
+      </section>
+      <section>
 
-  </main>
+      </section>
+    </main>
   );
-}
+};
+
+export default Landing;
---
+++ front/src/helpers/types/types.ts
@@ -0,0 +1,6 @@
+
+export interface BannerProps {
+    img: string;
+    text: string;
+    title: string;
+  }
