391a9f388020abd0c96686d14d675d72dceca76a
Luz NovCh <luznov06@gmail.com>
2024-08-21 08:36:37 -0300

    service button de home, a revisar

--- front/src/components/home/GarageHome.tsx
+++ front/src/components/home/GarageHome.tsx
@@ -24,9 +24,9 @@ const GarageHome = () => {
             <button className="custom-button">
               Buscar
             </button>
-            <Link href={PATHROUTES.LANDING}>
+            {/* <Link href={PATHROUTES.LANDING}>
               <button className="custom-button">Reservar</button>
-            </Link>
+            </Link> */}
           </div>
         </div>
 
