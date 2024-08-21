1c5116b753516f56a04b6f3d42b283888e772692
BlockBeam22Y <rodrigofc1910@gmail.com>
2024-08-20 19:26:05 -0500

    Configuracion de typeorm

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get('typeorm');
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
