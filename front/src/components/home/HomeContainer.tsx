6e261fd187675ce4a0e28419800b7c24c3ea2976
Luz NovCh <luznov06@gmail.com>
2024-08-22 06:56:14 -0300

    contacto vista lista

--- front/src/components/home/HomeContainer.tsx
+++ front/src/components/home/HomeContainer.tsx
@@ -1,11 +1,13 @@
 import HomeAbout from "./HomeAbout"
 import GarageHome from "./GarageHome"
+import HomeOpiniones from "./HomeOpiniones"
 
 
 const HomeContainer = () => {
     return <div>
         <GarageHome/>
         <HomeAbout/>
+        <HomeOpiniones/>
     </div>
 }
 
---
+++ front/src/components/home/HomeOpiniones.tsx
@@ -0,0 +1,10 @@
+const HomeOpiniones: React.FC = () => {
+  
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default HomeOpiniones;
